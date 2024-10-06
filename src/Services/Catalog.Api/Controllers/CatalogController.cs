1660c5dd0a1322c72c26fea4c04903af0d11c5fe
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2024-10-07 00:29:15 +0600

    crud operation

--- src/Services/Catalog.Api/Controllers/CatalogController.cs
+++ src/Services/Catalog.Api/Controllers/CatalogController.cs
@@ -3,6 +3,7 @@ using Catalog.Api.Model;
 using CoreApiResponse;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using System.Net;
 
 namespace Catalog.Api.Controllers
@@ -40,6 +41,85 @@ namespace Catalog.Api.Controllers
                 return CustomResult(ex.Message, HttpStatusCode.BadRequest);
             }
             
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.Created)]
+        public IActionResult CreateProduct([FromBody]Product product) 
+        {
+            try
+            {
+                product.Id = ObjectId.GenerateNewId().ToString();
+                bool isSaved = _productManager.Add(product);
+                if (isSaved) 
+                {
+                    return CustomResult("Product has been save Successfully.", product, HttpStatusCode.Created);
+                }
+                else
+                {
+                    return CustomResult("Product saved Fail.",HttpStatusCode.BadGateway);    
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public IActionResult UpdateProduct([FromBody] Product product)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(product.Id))
+                {
+                    return CustomResult("Data Not Found.", HttpStatusCode.NotFound);
+                }
+                bool isUpdate = _productManager.Update(product.Id, product);
+
+                if (isUpdate)
+                {
+                    return CustomResult("Product has been modify Successfully.", product, HttpStatusCode.OK);
+                }
+                else
+                {
+                    return CustomResult("Product modify Fail.", HttpStatusCode.BadGateway);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult DeleteProduct([FromBody] string id)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    return CustomResult("Data Not Found.", HttpStatusCode.NotFound);
+                }
+                bool isDelete = _productManager.Delete(id);
+
+                if (isDelete)
+                {
+                    return CustomResult("Product has been delete Successfully.", HttpStatusCode.OK);
+                }
+                else
+                {
+                    return CustomResult("Product delete Fail.", HttpStatusCode.BadGateway);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
         }
     }
 }
