580c863a5b948396fa9ec82defb6fed82e243d3d
Niaz Murshed Siam <56300948+niazmurshedsiam@users.noreply.github.com>
2024-10-18 11:55:49 +0600

    crud operation done

--- src/Services/Catalog.Api/Program.cs
+++ src/Services/Catalog.Api/Program.cs
@@ -15,11 +15,11 @@ builder.Services.AddScoped<IProductManager, ProductManager>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    //app.UseSwagger();
+//    //app.UseSwaggerUI();
+//}
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseAuthorization();
--- src/Services/Discount.API/Repository/CouponRepository.cs
+++ src/Services/Discount.API/Repository/CouponRepository.cs
@@ -11,21 +11,39 @@ namespace Discount.API.Repository
         {
             _configuration = configuration;
         }
-        public Task<bool> CreateDiscount(Coupon coupon)
+        public async Task<bool> CreateDiscount(Coupon coupon)
         {
-            throw new NotImplementedException();
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDB"));
+            var aff = await connection.ExecuteAsync("insert into coupon(ProductId, ProductName,Description,Amount) values(@ProductId, @ProductName,@Description,@Amount)",new { ProductId = coupon.ProductId, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount});
+            if (aff > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        public Task<bool> DeleteDiscount(string productId)
+        public async Task<bool> DeleteDiscount(string productId)
         {
-            throw new NotImplementedException();
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDB"));
+            var del = await connection.ExecuteAsync("delete from Coupon where ProductId = @ProductId", new { ProductId = productId });
+            if (del > 0) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public async Task<Coupon> GetDiscount(string productId)
         {
             var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDB"));
             var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
-                ("select 8 from coupon where ProductId = @ProductId", new { ProductId = productId });
+                ("select * from coupon where ProductId = @ProductId", new { ProductId = productId });
             if (coupon == null) 
             {
                 return new Coupon() { Amount = 0, ProductId = "No Discount" };
@@ -33,9 +51,18 @@ namespace Discount.API.Repository
             return coupon;
         }
 
-        public Task<bool> UpdateDiscount(Coupon coupon)
+        public async Task<bool> UpdateDiscount(Coupon coupon)
         {
-            throw new NotImplementedException();
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDB"));
+            var upp = await connection.ExecuteAsync("update Coupon set ProductId = @ProductId, ProductName = @ProductName,Description = @Description,Amount = @Amount", new { ProductId = coupon.ProductId, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+            if (upp > 0) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
