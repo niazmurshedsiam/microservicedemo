86e1a1bbb4e5fb376019f9df44b2d67ccc948b00
siam <niaz.siam.gakk@gmail.com>
2025-08-11 17:10:21 +0600

    Developing BuildingBlocks EventBus.Messages With RabbitMQ

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBusList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBusList
+    {
+        public const string BasketChecoutQueue = "basketcheckout-queue";
+    }
+}
