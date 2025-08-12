f7db04bd939e2f4453264ab4e17775b480d4bc81
siam <niaz.siam.gakk@gmail.com>
2025-08-12 11:40:53 +0600

    PostgreeSQL  connection string add

--- src/Services/Discounts.Grpc/Program.cs
+++ src/Services/Discounts.Grpc/Program.cs
@@ -1,10 +1,11 @@
+using Discount.Grpc.Repository;
 using Discounts.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
 // Add services to the container.
 builder.Services.AddGrpc();
 
