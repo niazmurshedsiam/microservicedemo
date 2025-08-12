3e03cfe7855c8383debc991e0aea183821240632
siam <niaz.siam.gakk@gmail.com>
2025-08-12 12:03:28 +0600

    Protobuf File for Exposing

--- src/Services/Discounts.Grpc/Program.cs
+++ src/Services/Discounts.Grpc/Program.cs
@@ -1,5 +1,5 @@
 using Discount.Grpc.Repository;
-using Discounts.Grpc.Services;
+//using Discounts.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +12,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
