b169f442cef5ac893153158064744dc86846346b
siam <niaz.siam.gakk@gmail.com>
2025-09-08 16:14:48 +0600

    Developing Ordering.Application Contracts in Clean Architecture

---
+++ src/Services/Ordering/Ordering.Application/Contacts/Persistence/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using EF.Core.Repository.Interface.Repository;
+using Ordering.Domain.Models;
+using System;
+
+
+namespace Ordering.Application.Contacts.Persistence
+{
+    public interface IOrderRepository:ICommonRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrderByUserName(string userName);
+    }
+}
