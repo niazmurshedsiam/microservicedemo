44bcbe9c15f053d6ac2b139a7152da552f00a3e4
siam <niaz.siam.gakk@gmail.com>
2025-09-10 17:17:16 +0600

    Developing Ordering.Application Layer- Application Features - DeleteOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contacts.Persistence;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler : IRequestHandler<DeleteOrderCommand, bool>
+    {
+        public IOrderRepository _repository;
+        public IMapper _mapper;
+        public DeleteOrderHandler(IOrderRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<bool> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+           
+            return await _repository.DeleteAsync(new Order() { Id = request.Id});
+        }
+    }
+}
