be102a6000ee443c12b38cc27a01a4fae460a4a6
siam <niaz.siam.gakk@gmail.com>
2025-09-10 16:32:03 +0600

    Developing Ordering.Application Layer- Application Features - Fluent Validation

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrderCommandValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands
+{
+    public class CreateOrderCommandValidation : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidation()
+        {
+            RuleFor(c => c.UserName).NotEmpty().WithMessage("Please Enter Your Name");
+            RuleFor(c => c.EmailAddress).NotEmpty().EmailAddress().WithMessage("Email Address Should be Valid");
+        }
+    }
+}
