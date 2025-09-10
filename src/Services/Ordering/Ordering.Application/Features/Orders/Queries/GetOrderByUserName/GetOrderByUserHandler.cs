d8168f9ace88dfede4347c1e9ad02428c10d6b6f
siam <niaz.siam.gakk@gmail.com>
2025-09-10 15:43:23 +0600

    Developing Ordering.Application Layer- Application Features - GetOrdersByUserName mediatr use

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderByUserName/GetOrderByUserHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contacts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderByUserName
+{
+    public class GetOrderByUserHandler : IRequestHandler<GetOrderByUserQuery, List<OrderVm>>
+    {
+        public IOrderRepository _orderRepository;
+        public IMapper _mapper;
+        public GetOrderByUserHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<OrderVm>> Handle(GetOrderByUserQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrderVm>>(orders);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderByUserName/GetOrderByUserQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderByUserName
+{
+    public class GetOrderByUserQuery:IRequest<List<OrderVm>>
+    {
+        public string UserName { get; set; }
+        public GetOrderByUserQuery(string userName) 
+        {
+            UserName = userName;
+        
+        }
+    }
+}
