75af3a21dd9ee4b783e33f53e9a516b4b64f74ce
siam <niaz.siam.gakk@gmail.com>
2025-09-11 13:03:11 +0600

    Developing Ordering Infrastructure Layer in Clean Architecture Persistence-DbContext Seed Method

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().HasData();
+            new Order
+            {
+                Id = 1,
+                UserName = "Test",
+                FirstName = "Test",
+                LastName = "Test",
+                EmailAddress = "test@gmail.com",
+                Address = "Test",
+                TotalPrice = 170,
+                City = "Dhaka"
+            };
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
@@ -11,7 +11,11 @@ namespace Ordering.Infrastructure.Persistence
     public class OrderDbContext:DbContext
     {
         public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
-
+        protected async override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            await OrderContextSeed.Seed(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Order> Orders { get; set; }
     }
 }
