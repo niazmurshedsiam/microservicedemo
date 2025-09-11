fbd1a79211da70a59c558e328113fdccdbf366eb
siam <niaz.siam.gakk@gmail.com>
2025-09-11 12:16:16 +0600

    Infrastructure Layer in Clean Architecture Persistence - Add DbContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderDbContext:DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
